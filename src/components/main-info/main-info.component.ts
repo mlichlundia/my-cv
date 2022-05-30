b3c5971d1ec04701f6b9429cb1885b0e7e04be64
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-05-30 20:51:47 +0200

    feat: make name field editable

--- src/components/main-info/main-info.component.ts
+++ src/components/main-info/main-info.component.ts
@@ -1,4 +1,3 @@
-import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from 'src/services/profile/profile.service';
 
@@ -9,13 +8,19 @@ import { ProfileService } from 'src/services/profile/profile.service';
 })
 export class MainInfoComponent implements OnInit {
   profileInfo = {
-    name: 'Ksenia Sychova',
+    name: '',
     profession: 'Junior Frontend Developer',
     summary: [
       'I`m a Junior Frontend Developer. I started to learn frontend development with my mentor. I think such way of learning helps me to understand more about working in company and also in a team. Our mentoring program increases my knowledge with the most important and modern Frontend technologies.',
       'I am looking for a company that will give me friendly enviroment in which i will increase my knowledge in creating large-scale applications.',
     ],
   };
+
+  isEditable: boolean = false;
+  hasError: boolean = false;
+
+  currentName: string = '';
+  currentClasses: Record<string, boolean> = {};
 
   constructor(private profileService: ProfileService) {}
 
@@ -27,5 +32,43 @@ export class MainInfoComponent implements OnInit {
     this.profileService.getProfile().subscribe((data) => {
       this.profileInfo.name = data.name;
     });
+  }
+
+  setCurrentClasses() {
+    this.currentClasses = {
+      profile__input_visible: this.isEditable,
+      profile__input_error: this.hasError,
+    };
+  }
+
+  onEdit(): void {
+    this.isEditable = true;
+    this.setCurrentClasses();
+
+    this.currentName = this.profileInfo.name;
+  }
+
+  onSave(isValid: boolean | null): void {
+    this.isEditable = false;
+
+    //check if name is invalid
+    if (!isValid) {
+      this.hasError = true;
+      this.setCurrentClasses();
+      return;
+    }
+    this.hasError = false;
+
+    this.setCurrentClasses();
+
+    //check if name was changed
+    if (this.currentName === this.profileInfo.name) {
+      return;
+    }
+
+    //make request
+    this.profileService
+      .setName(this.profileInfo.name)
+      .subscribe((message) => console.log(message));
   }
 }
