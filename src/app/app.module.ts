87f2d0294d497ac50167a86a580ead5d6e4cf0d2
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-06-13 11:04:52 +0200

    fix: rename interceptor exporter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,15 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from '../components/navigation/navigation.component';
-import { AppWrapperComponent } from '../components/app-wrapper/app-wrapper.component';
 import { HeaderComponent } from '../components/header/header.component';
 import { MainInfoComponent } from '../components/main-info/main-info.component';
 import { LoginPageComponent } from '../pages/login-page/login-page.component';
-import { httpIntrseptorProvider } from 'src/services/interseptors';
+import { httpIntrseptorProvider } from 'src/services/interseptors/InterseptorExport';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    AppWrapperComponent,
     HeaderComponent,
     MainInfoComponent,
     LoginPageComponent,
