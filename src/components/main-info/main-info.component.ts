74b90782407e079fe869b9e974a167d4a0d32b65
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-06-13 10:51:27 +0200

    fix: fix validation layout, delete unnecessary elements

--- src/components/main-info/main-info.component.ts
+++ src/components/main-info/main-info.component.ts
@@ -7,20 +7,12 @@ import { ProfileService } from 'src/services/profile/profile.service';
   styleUrls: ['./main-info.component.scss'],
 })
 export class MainInfoComponent implements OnInit {
-  profileInfo = {
-    name: '',
-    profession: 'Junior Frontend Developer',
-    summary: [
-      'I`m a Junior Frontend Developer. I started to learn frontend development with my mentor. I think such way of learning helps me to understand more about working in company and also in a team. Our mentoring program increases my knowledge with the most important and modern Frontend technologies.',
-      'I am looking for a company that will give me friendly enviroment in which i will increase my knowledge in creating large-scale applications.',
-    ],
-  };
+  username: string = '';
 
   isEditable: boolean = false;
   hasError: boolean = false;
 
-  currentName: string = '';
-  currentClasses: Record<string, boolean> = {};
+  currentNameame: string = '';
 
   constructor(private profileService: ProfileService) {}
 
@@ -30,45 +22,36 @@ export class MainInfoComponent implements OnInit {
 
   showProfile() {
     this.profileService.getProfile().subscribe((data) => {
-      this.profileInfo.name = data.name;
+      this.username = data.name;
     });
-  }
-
-  setCurrentClasses() {
-    this.currentClasses = {
-      profile__input_visible: this.isEditable,
-      profile__input_error: this.hasError,
-    };
   }
 
   onEdit(): void {
     this.isEditable = true;
-    this.setCurrentClasses();
-
-    this.currentName = this.profileInfo.name;
+    this.currentNameame = this.username;
   }
 
-  onSave(isValid: boolean | null): void {
+  onSave(valid: boolean | null): void {
     this.isEditable = false;
+    this.isValid(valid);
 
-    //check if name is invalid
-    if (!isValid) {
-      this.hasError = true;
-      this.setCurrentClasses();
-      return;
-    }
-    this.hasError = false;
-
-    this.setCurrentClasses();
-
-    //check if name was changed
-    if (this.currentName === this.profileInfo.name) {
+    //check if username was changed
+    if (this.currentNameame === this.username) {
       return;
     }
 
     //make request
     this.profileService
-      .setName(this.profileInfo.name)
+      .setName(this.username)
       .subscribe((message) => console.log(message));
+  }
+
+  isValid(valid: boolean | null) {
+    if (!valid) {
+      this.hasError = true;
+      return;
+    } else {
+      this.hasError = false;
+    }
   }
 }
