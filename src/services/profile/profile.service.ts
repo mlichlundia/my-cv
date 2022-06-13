cd0714c76d8a4527e71c9279cbab64cf8ba1598a
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-06-13 10:46:10 +0200

    fix: delete unnececssary error handler

--- src/services/profile/profile.service.ts
+++ src/services/profile/profile.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { retry } from 'rxjs/operators';
 import { GlobalVariables } from 'src/constants';
 import { Profile } from 'src/interfaces/profile';
 
@@ -14,25 +13,12 @@ export class ProfileService {
   constructor(private http: HttpClient) {}
 
   getProfile() {
-    return this.http
-      .get<Profile>(this.profileApi)
-      .pipe(retry(3), catchError(this.errorHandler));
+    return this.http.get<Profile>(this.profileApi).pipe(retry(3));
   }
 
   setName(name: string) {
     //to fix: change req object
     const body = { name, location: 'qwer', contacts: [] };
-    return this.http
-      .post<Profile>(this.profileApi, body)
-      .pipe(catchError(this.errorHandler));
-  }
-
-  private errorHandler(error: HttpErrorResponse) {
-    if (error.status === 0) {
-      console.error(error.error);
-    } else {
-      `Backend returned code: ${error.status}, body of the error: ${error.error}`;
-    }
-    return throwError(() => new Error());
+    return this.http.post<Profile>(this.profileApi, body);
   }
 }
