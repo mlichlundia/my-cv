98bfead956b8a6de6502acfb55dce6f62a14980c
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-09-29 19:59:05 +0200

    fix: replace services

--- src/app/services/article/article.service.ts
+++ src/app/services/article/article.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {map, Observable} from "rxjs";
-import {ARTICLE_URL} from "../../constants/url";
-import {ArticleInterface} from "../../interfaces/article.interface";
-import {ARTICLE_TOKEN} from "../../constants/token";
+import {ARTICLE_URL} from "../../modules/shared/constants/url";
+import {ArticleInterface} from "../../modules/shared/interfaces/article.interface";
+import {ARTICLE_TOKEN} from "../../modules/shared/constants/token";
 
 @Injectable({
   providedIn: 'root'
