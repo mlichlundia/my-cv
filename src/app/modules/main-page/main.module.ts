5c512b840c095a86d6db08b8f7eef09f9a25e2e0
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-09-29 19:43:43 +0200

    feat: implement template for home page

--- src/app/modules/main-page/main.module.ts
+++ src/app/modules/main-page/main.module.ts
@@ -5,6 +5,7 @@ import { MainPageComponent } from './main-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
 import { SkillsPageComponent } from './pages/skills-page/skills-page.component';
+import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
   declarations: [
@@ -13,6 +14,6 @@ import { SkillsPageComponent } from './pages/skills-page/skills-page.component';
     ProjectsPageComponent,
     SkillsPageComponent,
   ],
-  imports: [MainRoutingModule, CommonModule],
+  imports: [MainRoutingModule, CommonModule, SharedModule],
 })
 export class MainModule {}
