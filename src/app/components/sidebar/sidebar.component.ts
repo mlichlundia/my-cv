0f0f53fc1f41087a877554b370d39637c4c54f1e
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-09-30 22:37:21 +0200

    fix: fix menu logic

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,7 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, HostListener, Output} from '@angular/core';
 import {Contact} from "../../modules/shared/interfaces/contacts.interface";
 import {CONTACTS_MOCKS} from "../../modules/shared/mocks/contacts.mock";
-import {animate, state, style, transition, trigger} from "@angular/animations";
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-sidebar',
@@ -9,22 +9,45 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
   styleUrls: ['./sidebar.component.scss'],
   animations: [
     trigger( 'openClose', [
-      state('open', style( {transform: 'translate(0)'})),
-      state('close', style( {transform: 'translate(-23rem)'})),
-      transition('open <=> close', [ animate('400ms 0.2ms ease-in-out') ])
+      transition(':enter', [ style( {transform: 'translate(-23rem)', pointerEvents: 'none'}), animate('400ms 0.2ms ease-in-out', style( {transform: 'translate(0)', pointerEvents: 'all'})) ]),
+      transition(':leave', [ animate('400ms 0.2ms ease-in-out', style( {transform: 'translate(-23rem)', pointerEvents: 'none'})) ]),
       ]
     )
   ]
 })
 export class SidebarComponent{
+  @Output() public openMenu: EventEmitter<boolean> = new EventEmitter<boolean>()
+
   public contacts: Contact[] = CONTACTS_MOCKS
   public isOpen: boolean = true
 
-  public toggleOpen():void {
-    this.isOpen = !this.isOpen
+  @HostListener('window:click', ['$event'])
+  private closeModal() {
+    if(!this.isOpen) {
+      return
+    }
+
+    if(window.innerWidth >= 1440) {
+      return
+    }
+
+    if(
+      (event?.target as HTMLElement).closest('.sidebar-container')
+      && !(event?.target as HTMLElement).closest('ul li')
+    ) {
+      return
+    }
+
+    this.toggleOpen()
   }
 
-  public onResize(event: Event): void {
-    this.isOpen = (event.target as Window).innerWidth >= 1440;
+  @HostListener('window:resize', ['event$'])
+  private onResize(): void {
+    this.isOpen = (event?.target as Window).innerWidth >= 1440;
+  }
+
+  public toggleOpen():void {
+    this.isOpen = !this.isOpen
+    this.openMenu.emit(this.isOpen)
   }
 }
