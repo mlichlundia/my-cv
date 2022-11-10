0092881b193f30084fd22b714ff41b994dc3fbc1
mlichlundia <mlichlundiapl@gmail.com>
2022-11-10 19:16:05 +0100

    fix: replace img with icon component

--- src/app/modules/shared/components/sidebar-menu/social-link/social-link.component.ts
+++ src/app/modules/shared/components/sidebar-menu/social-link/social-link.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Contact} from "../../../interfaces/contacts.interface";
+import {Icons} from "../../UI/icon/constants";
 
 @Component({
   selector: 'app-social-link',
@@ -9,9 +10,10 @@ import {Contact} from "../../../interfaces/contacts.interface";
 export class SocialLinkComponent implements OnInit {
   @Input() public contact!: Contact
 
-  public imgSrc!: string
+  public icons: { [key:string]: any } = Icons
+  public currentIcon!: number
 
   ngOnInit(): void {
-    this.imgSrc = `/assets/svg/social/${this.contact.type}.svg`
+    this.currentIcon = this.icons[this.contact.type.toUpperCase()] || this.icons['DEFAULT_SOCIAL']
   }
 }
